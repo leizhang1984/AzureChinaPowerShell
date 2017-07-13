fdef68d5af14b99dd96e9e1cea47333aaa5b9752
leizhang1984 <kinkikid2005@hotmail.com>
2017-07-13 11:58:51 +0800

    20170713

---
+++ ARM/ExportVM/ExportVM.ps1
@@ -0,0 +1,74 @@
+#Created By MSFT leizhang (leizha@microsoft.com) on July 13th, 2017
+
+Clear-AzureProfile -Force
+
+$logarray=@()
+
+#For ARM Mode
+Add-AzureRmAccount -EnvironmentName AzureChinaCloud
+
+$SubscriptionNames = Get-AzureRMSubscription
+
+foreach ($sub in $SubscriptionNames)
+{
+        Select-AzureRMSubscription -SubscriptionName $sub.Name 
+
+        Write-Output "Processing " $sub.Name 
+
+        $vmlist = Get-AzureRMVM
+
+        foreach ($vm in $vmlist)
+        {   
+                #Get VM Status
+                $vmstatus = (get-azurermvm -ResourceGroupName $vm.ResourceGroupName -Name $vm.Name -Status).Statuses.DisplayStatus[1]
+
+                $output = new-object PSObject
+                $output | add-member -Membertype NoteProperty -Name "Mode" -value "ARM"
+                $output | add-member -Membertype NoteProperty -Name "SubscriptioName" -value "$($sub.Name)"
+                $output | add-member -Membertype NoteProperty -Name "ResourceGroupName" -value "$($vm.ResourceGroupName)"
+                $output | add-member -Membertype NoteProperty -Name "VMName" -value "$($vm.Name)"
+                $output | add-member -Membertype NoteProperty -Name "VMStatus" -value "$($vmstatus)"
+
+                $logarray += $output 
+                #add the current machinename, port and ACL to the array.               
+        }
+  }
+
+Write-Output "ARM is Done, prepare for ASM"
+
+#For ASM Mode
+Add-AzureAccount -Environment AzureChinaCloud
+
+$SubscriptionNames = Get-AzureSubscription
+
+foreach ($sub in $SubscriptionNames)
+{ 
+    Select-AzureSubscription -SubscriptionName $sub.SubscriptionName -Current
+
+    Write-Output "Processing " $sub.SubscriptionName
+
+    $vmlist = get-azureVM 
+
+    foreach($vm in $vmlist)
+    {
+            #Get VM Status
+            $vmstatus = $vm.status
+
+            $output = new-object PSObject
+            $output | add-member -Membertype NoteProperty -Name "Mode" -value "ASM"
+            $output | add-member -Membertype NoteProperty -Name "SubscriptioName" -value $sub.SubscriptionName
+            $output | add-member -Membertype NoteProperty -Name "ResourceGroupName" -value "ASM Default Resource Group"
+            $output | add-member -Membertype NoteProperty -Name "VMName" -value "$($vm.name)"
+            $output | add-member -Membertype NoteProperty -Name "VMStatus" -value "$($vmstatus)"
+               
+            $logarray += $output #add the current machinename, port and ACL to the array.
+    }
+}
+
+#write the logarray to a CSV file.
+$logArray | convertto-Csv -NoTypeInformation | out-file D:\azureVMList.csv -append -Encoding utf8 
+Write-Output "Export Success, please check azureacl file in Disk D:"
+
+
+
+
