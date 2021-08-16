d0f6e7055a04eba2e0d6c170d60c4f5c499856ef
Lei Zhang <kinkikid2005@hotmail.com>
2021-08-16 15:39:36 +0800

    2021-08-16

---
+++ AzModule/10ExportAADGroup/ExportAADGroup.ps1
@@ -0,0 +1,35 @@
+#Created By MSFT leizhang (leizha@microsoft.com) on 2021-08-16
+#Please run 
+#Install-Module -name AzureAD
+#for the first time
+
+$logarray=@()
+
+#login in to Azure
+Connect-AzureAD -AzureEnvironmentName AzureChinaCloud
+
+#Loop Subscriptions
+$exports = Get-AzureADGroup
+        
+foreach ($datarow in $exports)
+{
+        $output = new-object PSObject
+        $output | add-member -Membertype NoteProperty -Name "ObjectId" -value $datarow.ObjectId
+        $output | add-member -Membertype NoteProperty -Name "DisplayName" -value $datarow.DisplayName
+
+        if($datarow.dirsyncenabled -ne $null)
+        {
+            $output | add-member -Membertype NoteProperty -Name "dirsyncenabled" -value "Windows Server AD"
+            
+        }
+        else
+        {
+            $output | add-member -Membertype NoteProperty -Name "dirsyncenabled" -value "Azure Active Directory"
+        }
+
+        $logarray += $output 
+}
+    
+
+$logArray | convertto-Csv -NoTypeInformation | out-file D:\export_aadgroup.csv -append -Encoding utf8 
+Write-Output "Export AAD Account successfully"
